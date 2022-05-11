42e101f7198403e019d5c146cdd34922c49eb6f3
umingming <u_0112@naver.com>
2022-05-11 22:44:06 +0900

    Create MultiServer

---
+++ SocketProgram/src/kr/co/aim/tcp/MultiServer.java
@@ -0,0 +1,36 @@
+package kr.co.aim.tcp;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class MultiServer {
+	HashMap clients;
+	
+	MultiServer() {
+		clients = new HashMap();
+		Collections.synchronizedMap(clients);
+	}
+	
+	public void start() {
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
+		try {
+			serverSocket = new ServerSocket(7777);
+			System.out.println("서버 시작");
+			
+			while(true) {
+				socket = serverSocket.accept();
+				System.out.println("[" + socket.getInetAddress() + ":" + socket.getPort() + "] 에서 접속했습니다.");
+				ServerReceiver thread = new ServerReceiver(socket);
+				thread.start();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+}
