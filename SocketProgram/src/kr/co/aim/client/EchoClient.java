5a515d48b531c82d69f4f7703435fe2306ba6a6c
umingming <u_0112@naver.com>
2022-05-13 03:43:34 +0900

    Update client class

--- SocketProgram/src/kr/co/aim/client/EchoClient.java
+++ SocketProgram/src/kr/co/aim/client/EchoClient.java
@@ -1,19 +1,7 @@
 package kr.co.aim.client;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
-import kr.co.aim.client.MultiClient.ClientReceiver;
-import kr.co.aim.client.MultiClient.ClientSender;
 
 /*
 	에코 클라이언트
@@ -26,12 +14,6 @@ public class EchoClient {
 	private String msg;
 	private String ip;
 	private int port;
-	
-	private OutputStream out;
-	private InputStream in;
-	private PrintWriter sender;
-	private Scanner receiver;
-	private Scanner scanner;
 	
 	/*
 		생성자 정의
@@ -46,8 +28,6 @@ public class EchoClient {
 		
 		if(client != null) {
 			setClient();
-			communicate();
-			close();
 		}
 	}
 
@@ -65,7 +45,7 @@ public class EchoClient {
 	 */
 	private void accessServer() {
 		try {
-			scanner = new Scanner(System.in);
+			Scanner scanner = new Scanner(System.in);
 			System.out.print("[시스템 시작] IP 주소를 입력하세요. \n ☞ ");
 			ip = scanner.nextLine();
 			
@@ -89,64 +69,19 @@ public class EchoClient {
 	 */
 	private void setClient() {
 		try {
+			System.out.printf("[통신 시작] %s님 환영합니다.%n ☞ ", name);
+			
 			Thread sender = new Thread(new SenderThread(client, name));
 			Thread receiver = new Thread(new ReceiverThread(client));
 
 			sender.start();
 			receiver.start();
 			
-			out = client.getOutputStream();
-			sender = new PrintWriter(new OutputStreamWriter(out));
-	
-			in = client.getInputStream();
-			receiver = new Scanner(new InputStreamReader(in));
-			
-			sender.println(name);
-			System.out.printf("[통신 시작] %s님 환영합니다.%n ☞ ", name);
-			
-		} catch (IOException e) {
+		} catch (Exception e) {
 			System.out.println("[통신 실패]");
 		}
 	}
 
-	/*
-		communicate 메소드
-		1. while문 입력 값을 msg에 초기화 후 null이 아닐 경우 반복함.  
-			> if문 msg가 종료이면, break
-			> msg를 println으로 서버에 전송하고 flush 메소드로 확인함.
-			> 서버로부터 돌려 받은 값을 출력함.
-	 */
-	private void communicate() {
-		while((msg = scanner.nextLine()) != null) {
-			if(msg.equals("종료")) {
-				break;
-			}
-			
-			sender.println(msg);
-			sender.flush();
-			System.out.printf("%s%n ☞ ", receiver.nextLine());
-		}
-	}
-	
-	/*
-		close 메소드
-		1. 스트림과 소켓을 역순으로 닫음.
-		2. 접속 종료 여부를 안내함.
-	 */
-	private void close() {
-		try {
-			sender.close();
-			out.close();
-			receiver.close();
-			in.close();
-			client.close();
-			System.out.println("[서버 접속 종료]");
-			
-		} catch (IOException e) {
-			System.out.println("[접속 종료 실패]");
-		}
-	}
-	
 	/*
 		메인 메소드
 		1. 생성자 호출
---
+++ SocketProgram/src/kr/co/aim/client/ReceiverThread.java
@@ -0,0 +1,31 @@
+package kr.co.aim.client;
+
+import java.io.DataInputStream;
+import java.net.Socket;
+
+public class ReceiverThread implements Runnable {
+	private Socket socket;
+	private DataInputStream in;
+	
+	public ReceiverThread(Socket socket) {
+		this.socket = socket;
+		
+		try {
+			in = new DataInputStream(socket.getInputStream());
+		} catch(Exception e) {
+			System.out.println("[클라이언트 소켓 오류]");
+		}
+	}
+	
+
+	@Override
+	public void run() {
+		while(in != null) {
+			try {
+				System.out.println(in.readUTF());;
+			} catch(Exception e) {
+				System.out.println("[메시지 수신 오류]");
+			}
+		}
+	}
+}
