209fca3da571f75728842fc6440140c34c00dbc0
umingming <u_0112@naver.com>
2022-05-13 03:50:11 +0900

    Add annotation

--- SocketProgram/src/kr/co/aim/server/ClientGroup.java
+++ SocketProgram/src/kr/co/aim/server/ClientGroup.java
@@ -4,6 +4,9 @@ import java.io.DataOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ 	ClientGroup; 클라이언트들의 그룹을 정의함.
+ */
 public class ClientGroup {
 	private Map<String, DataOutputStream> map;
 	private String name;
@@ -21,15 +24,19 @@ public class ClientGroup {
 	public Map<String, DataOutputStream> getClientMap() {
 		return map;
 	}
+	
 	public void setClientMap(Map<String, DataOutputStream> clientMap) {
 		this.map = clientMap;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public boolean isFull() { 
 		return map.size() == total;
 	}
