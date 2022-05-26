3770c220cfafd837c409de62c3afa17c0c19a252
umingming <u_0112@naver.com>
2022-05-26 19:27:57 +0900

    Update project

---
+++ MultiGroupServer/src/kr/co/aim/data/Group.java
@@ -0,0 +1,13 @@
+package kr.co.aim.data;
+
+import java.time.LocalDateTime;
+
+/*
+ 	ClientGroup; 클라이언트들의 그룹을 정의함.
+ */
+public class Group {
+	private int seq;
+	private String name;
+	private int total;
+	private LocalDateTime regDate;
+}
