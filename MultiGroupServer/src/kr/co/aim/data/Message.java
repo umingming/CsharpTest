242611ab52ed1c9f601f429300a2948332369d73
umingming <u_0112@naver.com>
2022-05-26 19:32:10 +0900

    Update DTO Class

---
+++ MultiGroupServer/src/kr/co/aim/data/Message.java
@@ -0,0 +1,11 @@
+package kr.co.aim.data;
+
+import java.time.LocalDateTime;
+
+public class Message {
+	private int seq;
+	private int seqUser;
+	private int seqGroup;
+	private String content;
+	private LocalDateTime regDate;
+}
