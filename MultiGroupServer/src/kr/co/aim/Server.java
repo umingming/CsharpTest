8d6bc99a4b711246b9aafe13d510156be7076a68
umingming <u_0112@naver.com>
2022-05-26 20:09:40 +0900

    Create Thread class

--- MultiGroupServer/src/kr/co/aim/Server.java
+++
@@ -1,5 +0,0 @@
-package kr.co.aim;
-
-public class Server {
-
-}
---
+++ MultiGroupServer/src/kr/co/aim/service/ClientHandler.java
@@ -0,0 +1,10 @@
+package kr.co.aim.service;
+
+public class ClientHandler implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
