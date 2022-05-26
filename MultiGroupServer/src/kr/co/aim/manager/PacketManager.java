3388a4bd40527ec536616616df1b8cb0f9d62c13
umingming <u_0112@naver.com>
2022-05-26 20:20:31 +0900

    Add class

--- MultiGroupServer/src/kr/co/aim/manager/PacketManager.java
+++ MultiGroupServer/src/kr/co/aim/manager/PacketManager.java
@@ -1,5 +1,13 @@
 package kr.co.aim.manager;
 
+import java.net.Socket;
+
 public class PacketManager {
+	private Socket client;
+	
+	public PacketManager(Socket client) {
+		this.client = client;
+	}
+	
 
 }
--- MultiGroupServer/src/kr/co/aim/service/ClientHandler.java
+++ MultiGroupServer/src/kr/co/aim/service/ClientHandler.java
@@ -1,10 +1,36 @@
 package kr.co.aim.service;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import kr.co.aim.manager.GroupManager;
+import kr.co.aim.manager.PacketManager;
+
 public class ClientHandler implements Runnable {
+	private Socket client;
+	private GroupManager groupManager;
+	private PacketManager packetManager;
+	
+	private InputStream in;
+	private OutputStream out;
+	
+	
+	public ClientHandler(Socket client) {
+		this.client = client;
+		this.packetManager = new PacketManager(client);
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	public void addUser() {
 		
 	}
 }
