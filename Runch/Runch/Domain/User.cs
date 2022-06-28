1760bdd4f0938ba1af097a2863d856fa1b6a97a2
umingming <u_0112@naver.com>
2022-06-28 15:37:46 +0900

    Update code line

--- Runch/Runch/Domain/User.cs
+++ Runch/Runch/Domain/User.cs
@@ -30,6 +30,72 @@ namespace Runch.Domain
         }
 
         /*
+            IsValid
+            1. 유저 조회 쿼리 할당
+            2. if문 해당 유저가 있는지?
+                > id, name 저장
+                > true 반환
+         */
+        public Boolean IsValid(string id)
+        {
+            string sql = $@"select * from vwUserInfo where user_id = '{id}'";
+            using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
+            {
+                using (OleDbDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        id = reader["user_id"].ToString();
+                        Properties.Settings.Default.UserId = id;
+
+                        name = reader["name"].ToString();
+                        Properties.Settings.Default.UserName = name;
+
+                        Properties.Settings.Default.Save();
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+
+        /*
+            Add
+            1. insert 쿼리 선언
+            2. DB 작업 수행 
+         */
+        public void Add()
+        {
+            string sql = $@"insert into users values ('{id}', '{name}', {groupId}, {positionId})";
+            using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        /*
+            IsLoggedIn
+            1. 유저 조회 쿼리 할당
+            2. if문 값 읽기 
+                > if문 유저가 로그인 중인지?
+         */
+        public Boolean IsLoggedIn()
+        {
+            string sql = $@"select * from user_log where user_id = '{id}' and rownum = 1 order by user_log_id desc";
+            using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
+            {
+                using (OleDbDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        if (reader["log_type"].ToString() == "IN") return true;
+                    }
+                    return false;
+                }
+            }
+        }
+
+        /*
             Login
             1. if문 존재하는 사용자인지
             2. if문 로그인 된 사용자인지
@@ -90,72 +156,6 @@ namespace Runch.Domain
             using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
             {
                 if (!IsLoggedIn()) return;
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        /*
-            IsValid
-            1. 유저 조회 쿼리 할당
-            2. if문 해당 유저가 있는지?
-                > id, name 저장
-                > true 반환
-         */
-        public Boolean IsValid(string id)
-        {
-            string sql = $@"select * from vwUserInfo where user_id = '{id}'";
-            using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
-            {
-                using (OleDbDataReader reader = cmd.ExecuteReader())
-                {
-                    if (reader.Read())
-                    {
-                        id = reader["user_id"].ToString();
-                        Properties.Settings.Default.UserId = id;
-
-                        name = reader["name"].ToString();
-                        Properties.Settings.Default.UserName = name;
-
-                        Properties.Settings.Default.Save();
-                        return true;
-                    }
-                    return false;
-                }
-            }
-        }
-
-        /*
-            IsLoggedIn
-            1. 유저 조회 쿼리 할당
-            2. if문 값 읽기 
-                > if문 유저가 로그인 중인지?
-         */
-        public Boolean IsLoggedIn()
-        {
-            string sql = $@"select * from user_log where user_id = '{id}' and rownum = 1 order by user_log_id desc";
-            using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
-            {
-                using (OleDbDataReader reader = cmd.ExecuteReader())
-                {
-                    if (reader.Read())
-                    {
-                        if (reader["log_type"].ToString() == "IN") return true;
-                    }
-                    return false;
-                }
-            }
-        }
-
-        /*
-            Add
-            1. insert 쿼리 선언
-            2. DB 작업 수행 
-         */
-        public void Add()
-        {
-            string sql = $@"insert into users values ('{id}', '{name}', {groupId}, {positionId})";
-            using (OleDbCommand cmd = new OleDbCommand(sql, dbutil.Connect()))
-            {
                 cmd.ExecuteNonQuery();
             }
         }
